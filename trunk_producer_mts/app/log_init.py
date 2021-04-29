794a82d6f3558bc4fae28daf851ccfd7989d8d53
Vyacheslav Reysman <assimilation13cd@gmail.com>
2021-04-29 06:16:20 +0000

    Update datetime format

--- trunk_producer_mts/app/log_init.py
+++ trunk_producer_mts/app/log_init.py
@@ -9,6 +9,8 @@ def log_on(app_name):
     logger.propagate = False
     console_handler = logging.StreamHandler()
     logger.addHandler(console_handler)
-    formatter = logging.Formatter(fmt='%(levelname)s:[%(name)s]:%(message)s')
+    formatter = logging.Formatter(
+        fmt='%(asctime)s-%(levelname)s:[%(name)s]:%(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S')
     console_handler.setFormatter(formatter)
     return logger
