66e95ea974a51f4b1bcb7ac3b68177b4bd3a3c4e
Vyacheslav Reysman <assimilation13cd@gmail.com>
2021-05-20 21:45:32 +0000

    Update consumer.py

--- trunk_producer_mts/app/consumer.py
+++ trunk_producer_mts/app/consumer.py
@@ -1,16 +1,18 @@
-from threading import Thread
 from typing import Callable, Optional
+from threading import Thread
+import logging
 
 from pika import (
     BlockingConnection,
     ConnectionParameters,
 )
-from pika.adapters.blocking_connection import (
-    BlockingChannel
-)
 from pika.credentials import (
     PlainCredentials,
 )
+from pika.adapters.blocking_connection import (
+    BlockingChannel
+)
+from pika.spec import BasicProperties
 
 from conf import (
     RABBIT_HOST,
@@ -20,65 +22,57 @@ from conf import (
     RABBIT_VHOST)
 
 
+logging.getLogger("pika").setLevel(logging.WARNING)
+
+
 class RMQConsumer:
-    __connection: BlockingConnection
-    __channel: BlockingChannel
+    __cr: PlainCredentials
+    __cp: ConnectionParameters
     __thread: Thread
 
     def __init__(self) -> None:
-        self.__connect()
-
-    def __connect(self) -> None:
-        # pylint: disable=too-many-boolean-expressions
-        if (
-                not hasattr(self, '__connection')
-                or not self.__connection.is_open
-                or not self.__connection.is_closed
-                or not hasattr(self, '__channel')
-                or not self.__channel.is_open
-                or self.__channel.is_closed
-        ):
-            cr = PlainCredentials(
-                username=RABBIT_USERNAME,
-                password=RABBIT_PASS
-            )
-            cp = ConnectionParameters(
-                host=RABBIT_HOST,
-                port=RABBIT_PORT,
-                virtual_host=RABBIT_VHOST,
-                credentials=cr
-            )
-            self.__connection = BlockingConnection(
-                parameters=cp
-            )
-            self.__channel = self.__connection.channel()
+        self.__cr = PlainCredentials(
+            username=RABBIT_USERNAME,
+            password=RABBIT_PASS
+        )
+        self.__cp = ConnectionParameters(
+            host=RABBIT_HOST,
+            port=RABBIT_PORT,
+            virtual_host=RABBIT_VHOST,
+            credentials=self.__cr
+        )
 
     def consume(
-            self,
-            exchange: str,
-            queue: str,
-            func: Callable,
-            use_dead_ex: Optional[bool] = True,
-            durable: bool = True,
-            exchange_type: str = 'fanout',
+        self,
+        exchange: str,
+        queue: str,
+        func: Callable,
+        use_dead_ex: Optional[bool] = True,
+        durable: bool = True,
+        exchange_type: str = 'fanout',
     ) -> None:
+        connection = BlockingConnection(
+            parameters=self.__cp
+        )
+        channel = connection.channel()
+
         if use_dead_ex:
-            self.__channel.exchange_declare(
+            channel.exchange_declare(
                 exchange=f"{exchange}-dead",
                 exchange_type=exchange_type,
-                durable=True,
+                durable=durable,
             )
-            self.__channel.queue_declare(
+            channel.queue_declare(
                 queue=f"{queue}-dead",
                 durable=durable,
             )
-            self.__channel.queue_bind(
+            channel.queue_bind(
                 queue=f"{queue}-dead",
                 exchange=f"{exchange}-dead",
                 routing_key='',
             )
 
-        self.__channel.exchange_declare(
+        channel.exchange_declare(
             exchange=exchange,
             exchange_type=exchange_type,
             durable=True,
@@ -86,55 +80,64 @@ class RMQConsumer:
         arguments = {
             "x-dead-letter-exchange": f"{exchange}-dead",
         } if use_dead_ex else {}
-        self.__channel.queue_declare(
+        channel.queue_declare(
             queue=queue,
             durable=durable,
             arguments=arguments,
         )
-        self.__channel.queue_bind(
+        channel.queue_bind(
             queue=queue,
             exchange=exchange,
             routing_key='',
         )
-        self.__channel.basic_consume(
+        channel.basic_consume(
             queue=queue,
             on_message_callback=func,
         )
 
-        self.__thread = Thread(target=self._thread_run)
+        self.__thread = Thread(
+            target=self._thread_run,
+            args=(connection, channel),
+        )
         self.__thread.start()
 
-    def _thread_run(self) -> None:
+    def _thread_run(
+        self,
+        connection: BlockingConnection,
+        channel: BlockingChannel,
+    ) -> None:
         try:
-            self.__channel.start_consuming()
+            channel.start_consuming()
         except KeyboardInterrupt:
-            self.__channel.stop_consuming()
-        self.__connection.close()
+            channel.stop_consuming()
+        connection.close()
 
     def publish(
-            self,
-            exchange: str,
-            message: str,
-            use_dead_ex: Optional[bool] = True,
-            exchange_type: str = 'fanout',
+        self,
+        exchange: str,
+        message: str,
+        exchange_type: str = 'fanout',
+        persistent: bool = False,
+        durable: bool = True,
     ) -> None:
-        self.__connect()
+        connection = BlockingConnection(
+            parameters=self.__cp
+        )
+        channel = connection.channel()
 
-        if use_dead_ex:
-            self.__channel.exchange_declare(
-                exchange=f"{exchange}-dead",
-                exchange_type=exchange_type,
-                durable=True,
-            )
-
-        self.__channel.exchange_declare(
+        channel.exchange_declare(
             exchange=exchange,
             exchange_type=exchange_type,
-            durable=True,
+            durable=durable,
         )
 
-        self.__channel.basic_publish(
+        channel.basic_publish(
             exchange=exchange,
             routing_key='',
             body=message,
+            properties=BasicProperties(
+                delivery_mode=2 if persistent else 1
+            )
         )
+
+        connection.close()
